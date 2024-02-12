c245613ea744886c8167009a5a58fb928e1ded50
Harsh Sharma <97582983+harshbsharma@users.noreply.github.com>
2024-02-12 10:25:08 +0530

    Fixed database.ts for expose of mongoDB URL

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,23 +1,3 @@
-// import { MongoClient } from 'mongodb';
-
-// let cachedClient: MongoClient;
-
-// async function connectToDatabase() {
-//     if (cachedClient) {
-//         return cachedClient;
-//     }
-
-//     const client = new MongoClient("mongodb+srv://simplegoodsmart:AyVD6s40P1SP92K4@cluster0.p7mqnyg.mongodb.net/interntest");
-//     try {
-//         await client.connect();
-//         cachedClient = client;
-//         return client;
-//     } catch (error) {
-//         throw new Error('Failed to connect to MongoDB');
-//     }
-// }
-
-// export { connectToDatabase };
 
 
 import mongoose from 'mongoose';
